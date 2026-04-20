47c40adc7cab110cccd5b7ad0e8c42f9b140a628
Vavazzzz <nicola.vavassori@hotmail.it>
2026-04-20 14:14:36 +0200

    refactor: update Python version requirement and restructure main function in run.py

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import sys
 from pathlib import Path
 
-sys.path.insert(0, str(Path(__file__).parent / 'src'))
 
-from ui.main_window_refactored import main
+def main():
+    sys.path.insert(0, str(Path(__file__).parent / 'src'))
+    from ui.main_window_refactored import main as _main
+    _main()
+
 
 if __name__ == "__main__":
     main()
